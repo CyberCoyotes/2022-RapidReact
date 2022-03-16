25bb1047e9aae3cd5929e0915c8eb159e03317f2
Chad Marshall <cdmarshall25@gmail.com>
2022-03-16 17:44:34 -0400

    changed end condition on LaunchTAR

--- src/main/java/frc/robot/commands/Launcher/LaunchTarmac.java
+++ src/main/java/frc/robot/commands/Launcher/LaunchTarmac.java
@@ -42,6 +42,7 @@ public class LaunchTarmac extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    launcher.stopLauncher();
   }
 
   // Returns true when the command should end.
