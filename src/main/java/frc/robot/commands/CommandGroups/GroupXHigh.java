3f92b71d8c8cfd80054b5238831cc9e9bf1ad15f
Jestin VanScoyoc <coyotepod@gmail.com>
2022-03-22 14:36:00 -0400

    Simplified GroupXHigh
    Simplified the main portion of the Group X High shot by substituting Group2BallHigh

--- src/main/java/frc/robot/commands/CommandGroups/GroupXHigh.java
+++ src/main/java/frc/robot/commands/CommandGroups/GroupXHigh.java
@@ -1,12 +1,7 @@
 package frc.robot.commands.CommandGroups;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Launcher;
-import frc.robot.commands.IndexSpeed;
-import frc.robot.commands.IntakeSpeed;
-import frc.robot.commands.Launcher.LaunchSpeed;
 import frc.robot.commands.Launcher.xmode;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Index;
@@ -25,22 +20,11 @@ public class GroupXHigh extends SequentialCommandGroup{
   public GroupXHigh(Drivetrain m_drivetrain, Index indexMotors, Intake intakeMotor, Launcher launcher) {
       addRequirements(m_drivetrain, indexMotors);
       
-      addCommands( //for 2.05 seconds, run the launchers and xmode, giving the robot resistance to obstrusive pests.
-        new ParallelDeadlineGroup(
-          new WaitCommand(2.05),
-          new SequentialCommandGroup(
-        new LaunchSpeed(launcher, 0.35, 0.40).withTimeout(0.75),
-          new SequentialCommandGroup(
-            new LaunchSpeed(launcher, 0.35, 0.40).withTimeout(0.25).alongWith(
-              new IndexSpeed(indexMotors, 0.5).withTimeout(0.5)),
-                new ParallelCommandGroup (
-                  new LaunchSpeed(launcher, 0.36, 0.42),
-                  new IntakeSpeed(intakeMotor, 0.5),
-                  new IndexSpeed(indexMotors, 0.5).withTimeout(0.5))
-      )), new xmode(m_drivetrain))
-      );
-  }
+      addCommands( 
+        new Group2BallsHigh(launcher, intakeMotor, indexMotors),
+        // Aligns wheels into x pattern so robot isn't pushed around
+        new xmode(m_drivetrain) 
+      ); // end of commands
+  } // 
   
-    
-    
-}
+} // End class
