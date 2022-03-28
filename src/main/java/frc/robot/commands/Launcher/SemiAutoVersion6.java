29c817295b01e1454b7e84c18880b5692a6f928e
Jestin VanScoyoc <coyotepod@gmail.com>
2022-03-28 08:35:49 -0400

    Trial and error stuff for different SemiAuto files
    Creating separate 'trial and error' files for SemiAuto

---
+++ src/main/java/frc/robot/commands/Launcher/SemiAutoVersion6.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Launcher;
+
+/** Add your docs here. */
+public class SemiAutoVersion6 {}
+float Kp = -0.1f;
+float min_command = 0.05f;
+
+std::shared_ptr<NetworkTable> table = NetworkTable::GetTable("limelight");
+float tx = table->GetNumber("tx");
+
+if (joystick->GetRawButton(9))
+{
+        float heading_error = -tx;
+        float steering_adjust = 0.0f;
+        if (tx > 1.0)
+        {
+                steering_adjust = Kp*heading_error - min_command;
+        }
+        else if (tx < 1.0)
+        {
+                steering_adjust = Kp*heading_error + min_command;
+        }
+        left_command += steering_adjust;
+        right_command -= steering_adjust;
+}
