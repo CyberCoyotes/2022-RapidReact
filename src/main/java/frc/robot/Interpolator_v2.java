a6373b2a5ed3a384c8b9dc3a8d647d340d2ea165
Jestin VanScoyoc <coyotepod@gmail.com>
2022-04-10 09:30:44 -0400

    Attempt at breaking out of Limelight interpolation
    I don't think there is max or min value in the interpolation so the launcher just defaults to normal launching speed and never stops

--- src/main/java/frc/robot/Interpolator_v2.java
+++ src/main/java/frc/robot/Interpolator_v2.java
@@ -9,12 +9,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Interpolator {
+public class Interpolator_v2 {
 
     //This is an ordered list of "distance" measurements (this example is the y-angle reported by the Limelight).
     //This must be monotonically decreasing
 
-    // Find these values from Limelight
+    // Find these values from Limelight, tY
     private static double angles[] = {20.00, 18.00, 11.75, 2.00}; // 3.75 was max previously
     
     // The front and back launch motors need to have different speeds
@@ -27,6 +27,8 @@ public class Interpolator {
     // If it consistently shoots too low, this variable is used to correct that
     private static double manualOffset = 0;
 
+    // At some point would have max angle at which point the launch motors should not fire, i.e. speed = 0  
+    private static double maxAngle = 25; 
     /**
      * Static method that takes in the "distance" measurement and outputs "speed"
      * @param Distance measurement in "distance" units
@@ -40,15 +42,39 @@ public class Interpolator {
         while(angles[interAngle] > angle) {
             interAngle++;
 
+        // Added a Max Angle but this might not be needed with line of code in 'if' ~Scoy Comment
+        /** int interAngle = 0;
+         * while(angles[interAngle] > angle && angles[interAngle] < maxAngle ) {
+         * interAngle++;
+        **/
+
             //If it didn't find two points, return the last speed
+            
             if(interAngle >= angles.length) {
                 return speeds[speeds.length - 1];
+            
+            /** This might be where launcher should default to speed = 0 to break out of loop; 
+             * We have a manual firing button 
+             * so we don't want the robot fire at default speed ~Scoy Comment
+            **/
+
+            // if(interAngle >= angles.length) {
+                //return speeds[0];
+            
             }
         }
         
-        // Original code
-        if(interAngle == 0) {
-            return speeds[interAngle];        
+        /** Attempted fix to break out of loop
+         * Chance of heading a taget solution = exactly zero is very, very small
+         * 0 is more likely to be returned if there is no target acquired 
+         * 
+         * Original code
+         * if(interAngle == 0) {
+         * return speeds[interAngle];
+         **/
+
+        if(interAngle == 0 || interAngle > maxAngle) {
+            return speeds[0];
         }
 
         // Make a line from the points (angles[i-1], speeds[i-1]) and (angles[i], speeds[i])
