2ca257b6a1f70dc40164296239a676fa9c367284
Jestin VanScoyoc <coyotepod@gmail.com>
2022-04-13 09:42:12 -0400

    Contains the fixed Limelight

--- src/main/java/frc/robot/commands/Launcher/AdaptiveLaunch.java
+++ src/main/java/frc/robot/commands/Launcher/AdaptiveLaunch.java
@@ -32,13 +32,16 @@ public class AdaptiveLaunch extends CommandBase {
     if(limelight.hasValidTarget()) {
       double yAngle = limelight.getY();
       double speedFront = Interpolator.getFrontSpeed(yAngle);
+      System.out.println("Front launch output" + speedFront);
       double speedBack = Interpolator.getBackSpeed(yAngle);
+      System.out.println("Back launch output" + speedBack);
       subsystem.setLauncherSpeed(speedFront, speedBack);
     } else {
       subsystem.setLauncherSpeed(0.35, 0.40); // Split the tape launching, seems to be defaulting to this so that's good
     }
   }
 
+  
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
